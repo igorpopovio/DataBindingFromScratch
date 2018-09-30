8b7dc6f5e5a0e2f7e4919eca1900e06d3594024f
Igor Popov <igor@igorpopov.io>
2018-09-30 19:53:08 +0300

    Add view model
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ DataBindingFromScratch/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataBindingFromScratch
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModel()
+        {
+            Name = "Default name";
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
