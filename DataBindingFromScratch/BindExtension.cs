9bb4d70837626366b76c8c06aaa890b377d3a234
Igor Popov <igor@igorpopov.io>
2018-09-30 19:49:13 +0300

    Add test from markup extension
    Signed-off-by: Igor Popov <igor@igorpopov.io>

---
+++ DataBindingFromScratch/BindExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace DataBindingFromScratch
+{
+    public class BindExtension : MarkupExtension
+    {
+        public string Message { get; set; }
+
+        public BindExtension(string message)
+        {
+            Message = message;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Message;
+        }
+    }
+}
